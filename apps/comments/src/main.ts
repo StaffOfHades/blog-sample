f1bf93365c6e0e07e4b8d207e812eb1d046303d2
Mauricio Graciano <mauricio.graciano@teachable.com>
2021-12-23 13:07:50 -0600

    Add initial API implementation

--- apps/comments/src/main.ts
+++ apps/comments/src/main.ts
@@ -4,15 +4,33 @@
  */
 
 import * as express from 'express';
+import * as bodyParser from "body-parser"
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+app.use(bodyParser.json())
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to comments!' });
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  const { id: postId } = req.params
+  res.send(Object.values(commentsByPostId[postId] ?? {}));
 });
 
-const port = process.env.port || 3333;
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = uuidv4();
+  const { id: postId } = req.params
+  const { content } = req.body;
+  if (commentsByPostId[postId] === undefined) {
+    commentsByPostId[postId] = {}
+  }
+  commentsByPostId[postId][commentId] = { content, id: commentId };
+
+  res.status(201).send(Object.values(commentsByPostId[postId]))
+});
+
+const port = process.env.port || 4001;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/`);
 });
 server.on('error', console.error);
--- apps/posts/src/main.ts
+++ apps/posts/src/main.ts
@@ -4,15 +4,28 @@
  */
 
 import * as express from 'express';
+import * as bodyParser from "body-parser"
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+app.use(bodyParser.json())
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to posts!' });
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-const port = process.env.port || 3333;
+app.post('/posts', (req, res) => {
+  const id = uuidv4();
+  const { title } = req.body;
+  posts[id] = { id, title };
+
+  res.status(201).send(posts[id])
+});
+
+const port = process.env.port || 4000;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/`);
 });
 server.on('error', console.error);
